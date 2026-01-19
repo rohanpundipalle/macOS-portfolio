e41e6252f2166cd938823a352d631a599c0b99c7
Rohan <rohanpundipalle@gmail.com>
2026-01-19 22:43:39 +0530

    Added alias for imports settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "components"),
+      "@constants": resolve(__dirname, "constants"),
+      "@store": resolve(__dirname, "store"),
+      "@hoc": resolve(__dirname, "hoc"),
+      "@windows": resolve(__dirname, "windows"),
+    },
+  },
 });
