e35c2d1168b5b25f2208fc8afeb749c48e092013
Rohan <rohanpundipalle@gmail.com>
2026-01-26 17:11:54 +0530

    Implemented Safari Browser Window

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Dock, Navbar, Welcome } from "@components";
 import { Draggable } from "gsap/Draggable";
 import { gsap } from "gsap";
-import { Terminal } from "@windows";
+import { Safari, Terminal } from "@windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -13,6 +13,7 @@ const App = () => {
       <Dock />
 
       <Terminal />
+      <Safari />
     </main>
   );
 };
--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -19,7 +19,7 @@ const WindowWrapper = (Component, windowKey) => {
       gsap.fromTo(
         el,
         { opacity: 0, scale: 0.8, y: 40 },
-        { opacity: 1, scale: 1, duration: 0.4, ease: "power3.out", y: 0 },
+        { opacity: 1, scale: 1, y: 0, duration: 0.4, ease: "power3.out" },
       );
     }, [isOpen]);
 
